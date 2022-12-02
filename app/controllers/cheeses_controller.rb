0fddd5b623c41f2866940bba96d26829a181f5f4
allanstar-byte <allanmutisya263@gmail.com>
2022-12-02 09:39:41 +0300

    Custom rendering

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -3,13 +3,26 @@ class CheesesController < ApplicationController
   # GET /cheeses
   def index
     cheeses = Cheese.all
-    render json: cheeses
+    render json: cheeses, except: [:created_at, :updated_at]
   end
 
   # GET /cheeses/:id
   def show
+    # cheese = Cheese.find_by(id: params[:id])
+    # render json: {
+    #   id: cheese.id,
+    #   name: cheese.name,
+    #   price: cheese.price,
+    #   is_best_seller: cheese.is_best_seller
+    # }
+
     cheese = Cheese.find_by(id: params[:id])
-    render json: cheese
+    if cheese
+      render json: cheese, except: [:created_at, :updated_at], methods: [:summary]
+    else
+      render json: {error: "Couldn't find cheese"}, status: :not_found
+    end
+    
   end
 
 end
